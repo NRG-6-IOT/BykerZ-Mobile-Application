5a45a4457524670b1dd215b745f1646273ee9199
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-12 16:10:02 -0500

    feat(iam): implement profile service for user profile management.

---
+++ lib/iam/services/profile.service.dart
@@ -0,0 +1,22 @@
+import 'package:byker_z_mobile/shared/client/api.client.dart';
+import 'package:http/http.dart' as http;
+
+class ProfileService {
+  Future<http.Response> getProfile() async {
+    return await ApiClient.get('profiles/user');
+  }
+
+  Future<http.Response> assignOwnerToAssignment(String assignmentCode, int ownerId) async {
+    return await ApiClient.post('assignments/code/$assignmentCode/assign-owner/$ownerId', body: {
+      'ownerId': ownerId,
+    });
+  }
+
+  Future<http.Response> getOwnerId(String userId) async {
+    return await ApiClient.get('users/owner');
+  }
+
+  Future<http.Response> getMechanicId(String userId) async {
+    return await ApiClient.get('users/mechanic');
+  }
+}
