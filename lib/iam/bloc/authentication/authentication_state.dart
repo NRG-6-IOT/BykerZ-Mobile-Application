900199e1045cca7946258ccdc0e1b7a7755de77b
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-12 17:15:41 -0500

    feat(iam): add authentication state management for sign-in and sign-up.

---
+++ lib/iam/bloc/authentication/authentication_state.dart
@@ -0,0 +1,25 @@
+import 'package:byker_z_mobile/iam/models/sign-in_response.dart';
+
+abstract class AuthenticationState {}
+
+class AuthenticationInitial extends AuthenticationState {}
+
+class AuthenticationLoading extends AuthenticationState {}
+
+class SignInSuccess extends AuthenticationState {
+  final SignInResponse response;
+
+  SignInSuccess(this.response);
+}
+
+class SignUpSuccess extends AuthenticationState {
+  final SignUpSuccess response;
+
+  SignUpSuccess(this.response);
+}
+
+class AuthenticationFailure extends AuthenticationState {
+  final String error;
+
+  AuthenticationFailure(this.error);
+}
