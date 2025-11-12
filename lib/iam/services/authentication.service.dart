77daf1dd326544414d88ac8c6a182e4b04139d9e
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-12 15:21:50 -0500

    feat(api): implement API client with CRUD methods and token management.

--- lib/iam/services/authentication.service.dart
+++ lib/iam/services/authentication.service.dart
@@ -1,3 +1,4 @@
+import 'package:byker_z_mobile/shared/client/api.client.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/sign-in.request.dart';
@@ -5,15 +6,10 @@ import '../models/sign-up.request.dart';
 
 class AuthenticationService {
   Future<http.Response> signIn(SignInRequest request) async {
-    return await ApiClient.post(
-      'authentication/sign-in', body: request.toJson(),
-    );
+    return await ApiClient.post('authentication/sign-in', body: request.toJson());
   }
 
   Future<http.Response> signUp(SignUpRequest request) async {
-    return await ApiClient.post(
-      'authentication/sign-up',
-      body: request.toJson(),
-    );
+    return await ApiClient.post('authentication/sign-up', body: request.toJson(),);
   }
 }
