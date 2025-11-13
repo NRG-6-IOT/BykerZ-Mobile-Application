8bc34313dc80cc276f4a354d2810bea4a89b4526
Pachie <54517063+Pachieeee@users.noreply.github.com>
2025-11-12 19:01:45 -0500

    feat(profile): implement profile BLoC with fetch profile handling.

---
+++ lib/iam/bloc/profile/profile_state.dart
@@ -0,0 +1,21 @@
+import 'package:byker_z_mobile/iam/models/profile_entity.dart';
+
+abstract class ProfileState {}
+
+class ProfileInitial extends ProfileState {}
+
+class ProfileLoading extends ProfileState {}
+
+class ProfileLoaded extends ProfileState {
+  final Profile profile;
+
+  ProfileLoaded(this.profile);
+}
+
+class ProfileError extends ProfileState {
+  final String message;
+
+  ProfileError(this.message);
+}
+
+class ProfileNoMechanic extends ProfileState {}
