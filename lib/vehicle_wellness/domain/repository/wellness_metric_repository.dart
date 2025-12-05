d991e409631c47f36abaf22b19249f76b865641b
José Alejo Cárdenas <142996961+Firtness@users.noreply.github.com>
2025-12-05 06:33:18 -0500

    Add write operations to WellnessMetricRepository

--- lib/vehicle_wellness/domain/repository/wellness_metric_repository.dart
+++ lib/vehicle_wellness/domain/repository/wellness_metric_repository.dart
@@ -2,11 +2,11 @@ import '../entities/wellness_metric.dart';
 
 abstract class WellnessMetricRepository {
 
-  // OPERACIONES DE ESCRITURA (Commands) - LAS QUE FALTABAN
+  
   Future<WellnessMetric> createWellnessMetric(WellnessMetric metric);
   Future<WellnessMetric> updateWellnessMetric(int id, WellnessMetric metric);
   Future<void> deleteWellnessMetric(int id);
-  // OPERACIONES DE LECTURA (Queries)
+  
   Future<WellnessMetric> getWellnessMetricById(int id);
   Future<List<WellnessMetric>> getAllWellnessMetrics();
   Future<List<WellnessMetric>> getWellnessMetricsByVehicleId(int vehicleId);
